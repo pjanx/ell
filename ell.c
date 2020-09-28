13b4b8a5f5670d38eae095749ddbba734ac2df89
Přemysl Eric Janouch <p@janouch.name>
2020-09-28 05:11:26 +0200

    Name change

--- ell.c
+++ ell.c
@@ -1,7 +1,7 @@
 /*
  * ell.c: an experimental little language
  *
- * Copyright (c) 2017, Přemysl Janouch <p@janouch.name>
+ * Copyright (c) 2017, Přemysl Eric Janouch <p@janouch.name>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted.
